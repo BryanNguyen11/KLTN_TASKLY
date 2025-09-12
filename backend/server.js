b670893838b3c1776a53aa30a58419bde85a0f9b
huyhoang <huyhoangg191103@gmail.com>
2025-09-12 19:21:04 +0700

    init- 2

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes test
+app.get('/', (req, res) => {
+  res.send('Taskly API is running...');
+});
+
+// Kết nối MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('✅ MongoDB connected');
+    app.listen(process.env.PORT, () => {
+      console.log(`🚀 Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch(err => console.error(err));
